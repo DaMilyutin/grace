fe6d84696c7d079631d2e3b9b2719622f3c27b20
Danila Milyutin <dmilyutin@topcon.com>
2023-03-13 04:39:31 +0300

    thinking more generic annotations

--- include/grace/mediators/Path.h
+++ include/grace/mediators/Path.h
@@ -4,23 +4,45 @@
 
 namespace grace
 {
-    using Path = std::vector<Point_r>;
+    using PathPoints = std::vector<Point_r>;
 
     namespace annotations
     {
         struct Distance
         {
-            real_t distance = 0.0f;
+            Distance() = default;
+            Distance(real_t d): distance(d) {}
+            Distance(Vector_r const& v): distance(norm(v)) {}
+            Distance(Vector_r const& v, Distance const&): distance(norm(v)) {}
 
-            static Distance from(Vector_r const& v) { return Distance{norm(v)}; }
+            real_t distance = 0.0f;
         };
 
-        struct DistanceDirection
+        struct Direction
         {
-            real_t distance = 0.0f;
-            real_t direction = 0.0f;
+            Direction() = default;
+            Direction(real_t d): direction(d) {}
+            Direction(Vector_r const& v): direction(atan2(v.y, v.x)) {}
+            Direction(Vector_r const& v, Direction const&): direction(atan2(v.y, v.x)) {}
 
-            static DistanceDirection from(Vector_r const& v) { return DistanceDirection{norm(v), atan2(v.y, v.x)}; }
+            real_t direction = 0.0f;
+        };
+
+        struct Path
+        {
+            Path() = default;
+
+            Path(real_t p): path(p) {}
+            Path(Vector_r const& v, Path prev = Path()): path(norm(v) + prev.path) {}
+            Path(Distance d, Path prev = Path()): path(d.distance + prev.path) {}
+
+            real_t path = 0.0f;
+        };
+
+        template<typename... Ts>
+        struct United: Ts...
+        {
+            static United from(Vector_r const& v, real_t prev = 0.0f) { return ; }
         };
 
         template<typename S, typename A>
@@ -38,14 +60,14 @@ namespace grace
             PathAnnotation(PathAnnotation const&) = default;
             PathAnnotation(PathAnnotation&&) = default;
 
-            PathAnnotation(Path const& p)
+            PathAnnotation(PathPoints const& p)
             {
                 data.resize(p.size());
                 if(data.size() == 0)
                     return;
                 data[0] = E{};
                 for(agge::count_t i = 1; i < p.size(); ++i)
-                    data[i] = E::from(p[i] - p[i-1]);
+                    data[i] = E(p[i] - p[i-1]);
             }
 
             void clear() { data.clear(); }
